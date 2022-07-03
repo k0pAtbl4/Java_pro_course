7ae6d55e274babdbd66f7f356f368566e3113d6b
sasha <sasharadiu@gmail.com>
2022-07-03 19:41:06 +0300

    commit

---
+++ src/main/java/lesson2/task2.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class task2 {
+    public static void main(String[] args) {
+        if(args.length != 0) {
+            for(int i = args.length - 1; i >= 0; i--) {
+                System.out.print(args[i] + " ");
+            }
+        }
+    }
+}
