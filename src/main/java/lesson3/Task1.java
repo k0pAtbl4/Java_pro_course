8ca52ed0bac8b01659c59729821a36235f0ce1ac
sasha <sasharadiu@gmail.com>
2022-07-06 13:59:23 +0300

    commit

---
+++ src/main/java/lesson3/Task1.java
@@ -0,0 +1,19 @@
+package lesson3;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Even ones:");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+        System.out.println("\nOdd ones:");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+}
---
+++ src/main/java/lesson3/Task4.java
@@ -0,0 +1,18 @@
+package lesson3;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 2, 3, 4, 6, 25, 736, 3, 678, 12, -7, 57, 891, 45};
+        int min = numbers[0];
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Max: " + max + "\nMin: " + min);
+    }
+}
